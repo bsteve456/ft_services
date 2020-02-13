7c1c96996d0c7a2e7705d89574756087562945bf
Steve-alan BALEBA BALEBA <stbaleba@e1r10p23.42.fr>
2020-02-13 14:24:41 +0100

    add nginx

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 export MINIKUBE_HOME=~/goinfre
 minikube config set vm-driver virtualbox
 minikube start
+kubectl create -f /Users/stbaleba/42_Circle_3/github_service/srcs/ingress.yml
+minikube addons enable ingress
 minikube dashboard
