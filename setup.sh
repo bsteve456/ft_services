c3634961519f3cb7e521de1f760b1c19c8cb073d
Steve-alan BALEBA BALEBA <stbaleba@e1r10p13.42.fr>
2020-02-19 16:27:58 +0100

    link ingress to ftps-service

--- setup.sh
+++ setup.sh
@@ -4,9 +4,11 @@ minikube start
 minikube docker-env
 eval $(minikube -p minikube docker-env)
 bash srcs/ssl.sh
+bash srcs/docker_alpine/ftps_ssl.sh
 kubectl create -f /Users/stbaleba/42_Circle_3/github_service/srcs/ingress.yml
 minikube addons enable ingress
 kubectl apply -f srcs/service-nginx.yml
 docker build -t test_alpine ./srcs/docker_alpine/
 kubectl apply -f srcs/ftps.yml
+rm /Users/stbaleba/42_Circle_3/github_service/srcs/docker_alpine/vsftpd.pem
 minikube dashboard
--- srcs/docker_alpine/start_ftps.sh
+++ srcs/docker_alpine/start_ftps.sh
@@ -1,3 +1,4 @@
 #! bin/bash
 
+ls -l /home/steve && ls -l /home/
 rc-service vsftpd restart && tail -f /dev/null
