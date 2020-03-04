5d01fe5777f37383f5a07208381ec0c6423d6fbb
Steve-alan BALEBA BALEBA <stbaleba@e1r10p13.42.fr>
2020-03-04 14:23:44 +0100

    nginx own container create

--- setup.sh
+++ setup.sh
@@ -4,16 +4,17 @@ minikube start
 minikube docker-env
 eval $(minikube -p minikube docker-env)
 export MINI_IP=`minikube ip`
-touch pasv.txt | echo "pasv_address=" | tee pasv.txt
-sed -i '' -e '$ d' srcs/docker_alpine/vsftpd.conf
-sed "/^pasv_address=/ s/\$/$MINI_IP/" pasv.txt | tee pasv.txt | tee -a srcs/docker_alpine/vsftpd.conf
+touch pasv.txt
+echo "pasv_address=" | tee pasv.txt | sed "/^pasv_address=/ s/\$/$MINI_IP/" pasv.txt | tee pasv.txt | tee -a srcs/docker_alpine/vsftpd.conf
 rm pasv.txt
-bash srcs/ssl.sh
+#bash srcs/ssl.sh
 #bash srcs/docker_alpine/ftps_ssl.sh
 kubectl create -f srcs/ingress.yml
 minikube addons enable ingress
+docker build -t nginx-alpine ./srcs/nginx_alpine/
 kubectl apply -f srcs/service-nginx.yml
 docker build -t test_alpine ./srcs/docker_alpine/
 kubectl apply -f srcs/ftps.yml
+sed -i '' -e '$ d' srcs/docker_alpine/vsftpd.conf
 #rm srcs/docker_alpine/vsftpd.pem
 minikube dashboard
