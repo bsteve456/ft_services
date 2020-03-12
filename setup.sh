6dd973560112ccd7dc339294f7069ee8c691b624
Steve-alan BALEBA BALEBA <stbaleba@e1r10p14.42.fr>
2020-03-12 11:44:14 +0100

    setup check for ftps

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
+#! bin/bash
+
 export MINIKUBE_HOME=~/goinfre
 minikube config set vm-driver virtualbox
 minikube start
 minikube docker-env
 eval $(minikube -p minikube docker-env)
-export MINI_IP=`minikube ip`
 touch pasv.txt
-echo "pasv_address=" | tee pasv.txt | sed "/^pasv_address=/ s/\$/$MINI_IP/" pasv.txt | tee pasv.txt | tee -a srcs/docker_alpine/vsftpd.conf
+echo "pasv_address=" | tee pasv.txt | sed "/^pasv_address=/ s/\$/$(minikube ip)/" pasv.txt | tee -a srcs/docker_alpine/vsftpd.conf
 rm pasv.txt
-#bash srcs/ssl.sh
-#bash srcs/docker_alpine/ftps_ssl.sh
 kubectl create -f srcs/ingress.yml
 minikube addons enable ingress
 docker build -t nginx-alpine ./srcs/nginx_alpine/
@@ -16,5 +15,4 @@ kubectl apply -f srcs/service-nginx.yml
 docker build -t test_alpine ./srcs/docker_alpine/
 kubectl apply -f srcs/ftps.yml
 sed -i '' -e '$ d' srcs/docker_alpine/vsftpd.conf
-#rm srcs/docker_alpine/vsftpd.pem
 minikube dashboard
