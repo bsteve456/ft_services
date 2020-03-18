cfb9bf8ac303b68823c19ef1f542aee2561ffddc
bsteve456 <stevealan1@protonmail.com>
2020-03-18 21:43:06 +0100

    add wordpress service and deplyment without msql

--- setup.sh
+++ setup.sh
@@ -1,18 +1,20 @@
 #! bin/bash
 
-export MINIKUBE_HOME=~/goinfre
+#export MINIKUBE_HOME=~/goinfre
 minikube config set vm-driver virtualbox
 minikube start
 minikube docker-env
 eval $(minikube -p minikube docker-env)
-touch pasv.txt
-echo "pasv_address=" | tee pasv.txt | sed "/^pasv_address=/ s/\$/$(minikube ip)/" pasv.txt | tee -a srcs/docker_alpine/vsftpd.conf
-rm pasv.txt
+#touch pasv.txt
+#echo "pasv_address=" | tee pasv.txt | sed "/^pasv_address=/ s/\$/$(minikube ip)/" pasv.txt | tee -a srcs/docker_alpine/vsftpd.conf
+#rm pasv.txt
 kubectl create -f srcs/ingress.yml
 minikube addons enable ingress
 docker build -t nginx-alpine ./srcs/nginx_alpine/
 kubectl apply -f srcs/service-nginx.yml
-docker build -t test_alpine ./srcs/docker_alpine/
-kubectl apply -f srcs/ftps.yml
-sed -i '' -e '$ d' srcs/docker_alpine/vsftpd.conf
+#docker build -t test_alpine ./srcs/docker_alpine/
+#kubectl apply -f srcs/ftps.yml
+#sed -i '' -e '$ d' srcs/docker_alpine/vsftpd.conf
+docker build -t wordpress_alpine ./srcs/wordpress/
+kubectl apply -f srcs/wordpress/wordpress.yml
 minikube dashboard
