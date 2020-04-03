18972ae8141bfbcb5bf552837daea845eb845425
bsteve456 <stevealan1@protonmail.com>
2020-04-03 17:23:05 +0200

    add wordpress

--- setup.sh
+++ setup.sh
@@ -2,19 +2,22 @@
 
 #export MINIKUBE_HOME=~/goinfre
 minikube config set vm-driver virtualbox
-minikube start
+minikube start --vm-driver=virtualbox
 minikube docker-env
 eval $(minikube -p minikube docker-env)
 #touch pasv.txt
 #echo "pasv_address=" | tee pasv.txt | sed "/^pasv_address=/ s/\$/$(minikube ip)/" pasv.txt | tee -a srcs/docker_alpine/vsftpd.conf
 #rm pasv.txt
-kubectl create -f srcs/ingress.yml
-minikube addons enable ingress
-docker build -t nginx-alpine ./srcs/nginx_alpine/
-kubectl apply -f srcs/service-nginx.yml
+#kubectl create -f srcs/ingress.yml
+#minikube addons enable ingress
+#docker build -t nginx-alpine ./srcs/nginx_alpine/
+#kubectl apply -f srcs/service-nginx.yml
 #docker build -t test_alpine ./srcs/docker_alpine/
 #kubectl apply -f srcs/ftps.yml
 #sed -i '' -e '$ d' srcs/docker_alpine/vsftpd.conf
 docker build -t wordpress_alpine ./srcs/wordpress/
-kubectl apply -f srcs/wordpress/wordpress.yml
+#kubectl apply -f srcs/wordpress/wordpress.yml
+docker build -t mysql_alpine ./srcs/mysql/
+#kubectl apply -f srcs/mysql/mysql.yml
+kubectl apply -k ./srcs/
 minikube dashboard
--- srcs/wordpress/start.sh
+++ srcs/wordpress/start.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 
+mv /usr/share/wordpress/* /var/www/localhost/htdocs/wordpress/
+mysql -h mysql -u root -ppassword -e 'CREATE DATABASE IF NOT EXISTS wordpres
+s';
 rc-service php-fpm7 start
 tail -f /dev/null
