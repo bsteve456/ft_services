d275f83efe355b6e503573087084ff3e82e9a3eb
bsteve456 <stevealan1@protonmail.com>
2020-04-05 15:50:12 +0200

    change dir names

--- setup.sh
+++ setup.sh
@@ -10,9 +10,9 @@ eval $(minikube -p minikube docker-env)
 #rm pasv.txt
 #kubectl create -f srcs/ingress.yml
 #minikube addons enable ingress
-#docker build -t nginx-alpine ./srcs/nginx_alpine/
+#docker build -t nginx-alpine ./srcs/nginx/
 #kubectl apply -f srcs/service-nginx.yml
-#docker build -t test_alpine ./srcs/docker_alpine/
+#docker build -t test_alpine ./srcs/ftps/
 #kubectl apply -f srcs/ftps.yml
 #sed -i '' -e '$ d' srcs/docker_alpine/vsftpd.conf
 docker build -t wordpress_alpine ./srcs/wordpress/
