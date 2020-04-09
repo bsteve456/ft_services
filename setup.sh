6cf555ee6dbd990997ad736ce96109af7a98d4f7
bsteve456 <stevealan1@protonmail.com>
2020-04-09 16:41:55 +0200

    mini_ip auto add to telegraf

--- setup.sh
+++ setup.sh
@@ -6,8 +6,9 @@ minikube start --memory 3000 --bootstrapper=kubeadm
 minikube docker-env
 eval $(minikube -p minikube docker-env)
 #touch pasv.txt
-#echo "pasv_address=" | tee pasv.txt | sed "/^pasv_address=/ s/\$/$(minikube ip)/" pasv.txt | tee -a srcs/docker_alpine/vsftpd.conf
+#echo "pasv_address=" | tee pasv.txt | sed "/^pasv_address=/ s/\$/$(minikube ip)/" pasv.txt | tee -a srcs/ftps/vsftpd.conf
 #rm pasv.txt
+sed -i "s/mini_ip/$(minikube ip)/" srcs/telegraf/telegraf.conf
 kubectl create -f srcs/ingress.yaml
 minikube addons enable ingress
 minikube addons enable metrics-server
@@ -15,7 +16,7 @@ minikube addons enable metrics-server
 #kubectl apply -f srcs/service-nginx.yml
 #docker build -t test_alpine ./srcs/ftps/
 #kubectl apply -f srcs/ftps.yml
-#sed -i '' -e '$ d' srcs/docker_alpine/vsftpd.conf
+#sed -i '' -e '$ d' srcs/ftps/vsftpd.conf
 docker build -t wordpress_alpine ./srcs/wordpress/
 #kubectl apply -f srcs/wordpress/wordpress.yml
 docker build -t mysql_alpine ./srcs/mysql/
@@ -25,6 +26,7 @@ docker build -t influxdb_alpine ./srcs/influxdb/
 docker build -t telegraf_alpine ./srcs/telegraf/
 docker build -t grafana_alpine ./srcs/grafana/
 kubectl apply -k ./srcs/
+sed -i "s/$(minikube ip)/mini_ip/" srcs/telegraf/telegraf.conf
 #kubectl delete -n default deployment wordpress
 #kubectl apply -f srcs/wordpress-deployment.yaml
 minikube dashboard
