7d3cf662c6a7a674a0642b3b115ec8a5408491da
bsteve456 <stevealan1@protonmail.com>
2020-04-09 22:05:28 +0200

    ft_service done

--- setup.sh
+++ setup.sh
@@ -8,13 +8,14 @@ eval $(minikube -p minikube docker-env)
 #touch pasv.txt
 #echo "pasv_address=" | tee pasv.txt | sed "/^pasv_address=/ s/\$/$(minikube ip)/" pasv.txt | tee -a srcs/ftps/vsftpd.conf
 #rm pasv.txt
+sed -i "s/mini_ip/$(minikube ip)/" srcs/ftps/vsftpd.conf
 sed -i "s/mini_ip/$(minikube ip)/" srcs/telegraf/telegraf.conf
 kubectl create -f srcs/ingress.yaml
 minikube addons enable ingress
 minikube addons enable metrics-server
-#docker build -t nginx-alpine ./srcs/nginx/
+docker build -t nginx-alpine ./srcs/nginx/
 #kubectl apply -f srcs/service-nginx.yml
-#docker build -t test_alpine ./srcs/ftps/
+docker build -t ftps_alpine ./srcs/ftps/
 #kubectl apply -f srcs/ftps.yml
 #sed -i '' -e '$ d' srcs/ftps/vsftpd.conf
 docker build -t wordpress_alpine ./srcs/wordpress/
@@ -27,6 +28,7 @@ docker build -t telegraf_alpine ./srcs/telegraf/
 docker build -t grafana_alpine ./srcs/grafana/
 kubectl apply -k ./srcs/
 sed -i "s/$(minikube ip)/mini_ip/" srcs/telegraf/telegraf.conf
+sed -i "s/$(minikube ip)/mini_ip/" srcs/ftps/vsftpd.conf
 #kubectl delete -n default deployment wordpress
 #kubectl apply -f srcs/wordpress-deployment.yaml
 minikube dashboard
